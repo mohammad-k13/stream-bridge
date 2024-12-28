dd630da26a0e29b96c02a564026bd41a7336ec5a
Mohammad <mohammadkeshavarz.a.13@gmail.com>
2024-12-28 22:28:02 +0330

    store selectedChat

--- app/chat/page.tsx
+++ app/chat/page.tsx
@@ -2,7 +2,10 @@ import React from 'react'
 
 const page = () => {
   return (
-    <div>page</div>
+    <section className='w-full h-full'>
+      <header className='w-full h-14 bg-rose-100'></header>
+
+    </section>
   )
 }
 
