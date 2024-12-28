ad359ef4f4cdc2ff92fcba9c11192332b45adf67
Mohammad <mohammadkeshavarz.a.13@gmail.com>
2024-12-28 22:36:10 +0330

    show selected chat

--- app/chat/page.tsx
+++ app/chat/page.tsx
@@ -1,12 +1,26 @@
-import React from 'react'
+"use client";
+
+import useChatListStore from "@/store/chat/chat-list/useChatListStore";
+import Image from "next/image";
+import React from "react";
 
 const page = () => {
-  return (
-    <section className='w-full h-full'>
-      <header className='w-full h-14 bg-rose-100'></header>
+    const { selectedChat } = useChatListStore();
+    if (!selectedChat) return <p>page</p>;
 
-    </section>
-  )
-}
+    return (
+        <section className="w-full h-full">
+            <header className="w-full h-14 flex items-center justify-between p-2 border-b-[1px] border-gray-secondary">
+                <div className="flex items-start justify-center gap-5">
+                    <Image src={selectedChat.profileUrl} width={48} height={48} alt="profile-iamge" />
+                    <div className="flex flex-col items-start justify-center">
+                        <h4 className="font-semibold text-heading-4">{selectedChat.username}</h4>
+                        <p className="text-gray text-caption">last seen recently</p>
+                    </div>
+                </div>
+            </header>
+        </section>
+    );
+};
 
-export default page
+export default page;
