89f462bbed19aeae95622210deabd4e439833693
Mohammad <mohammadkeshavarz.a.13@gmail.com>
2024-12-31 20:33:42 +0330

    update routes

--- app/chat/page.tsx
+++ app/chat/page.tsx
@@ -1,8 +1,10 @@
 "use client";
 
+import { Button } from "@/components/ui/button";
 import useChatListStore from "@/store/chat/chat-list/useChatListStore";
 import Image from "next/image";
 import React from "react";
+import Cookies from 'js-cookie'
 
 const Page = () => {
     const { selectedChat } = useChatListStore();
